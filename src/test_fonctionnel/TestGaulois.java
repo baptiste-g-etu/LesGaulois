f6ab4eb2abb9b353c6df48c97170daa5d2e89891
unknown <baptiste.goudet@gmail.com>
2025-09-29 18:52:11 +0200

    TP 1 POO : Fin du point 1

--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -1,5 +1,13 @@
 package test_fonctionnel;
 
-public class TestGaulois {
+import personnages.Gaulois;
 
+public class TestGaulois {
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		Gaulois obelix = new Gaulois("Obélix", 16);
+		asterix.parler("Bonjour Obélix.");
+		obelix.parler("Bonjour Astérix. Ca (rip UTF) te dirais d'aller chasser des sangliers ? ");
+		asterix.parler("Oui, très bonne idée.");
+	}
 }
