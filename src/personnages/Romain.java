1c0a15848383fd85cbb2acd1f64b46e41947e257
Nerf Magicarpe <baptiste.goudet@gmail.com>
2025-11-03 18:44:33 +0100

    Add TP2 - Part 3. END OF TP 2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,9 +1,13 @@
 package personnages;
+import objets.Equipement;
 
 public class Romain {
 	//Attributs
 	private String nom;
 	private int force;
+	
+	Equipement[] equipements = new Equipement[2];
+	int nbEquipements = 0;
 	
 	//Constructeur
 	public Romain(String nom, int force) {
@@ -45,7 +49,7 @@ public class Romain {
 	}
 	
 	
-	// ----------- TP 2 -----------
+	// ----------- TP 2 - Partie 2 -----------
 	private boolean isInvariantVerified(int force) {
 		return force >= 0;
 	}
@@ -54,6 +58,43 @@ public class Romain {
 		Romain minus = new Romain("Minus", 6);
 		Gaulois asterix = new Gaulois("Astérix", 4);
 		asterix.frapper(minus);
+		
+		// ----------- TP 2 - Partie 3 -----------
+		Equipement casque = Equipement.CASQUE;
+		Equipement bouclier = Equipement.BOUCLIER;
+		
+		minus.sEquiper(casque);
+		minus.sEquiper(casque);
+		minus.sEquiper(bouclier);
+		minus.sEquiper(bouclier);
+		
+	}
+
+	
+	public void sEquiper(Equipement equipement) {
+		switch(nbEquipements) {
+			case 2:
+				System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+				break;
+			case 1:
+				if(equipements[0] == equipement) {
+					System.out.println("Le soldat " + nom + " possède déjà un " + equipement.toString() + " !");
+				}else {
+					ajouterEquipement(equipement);
+					System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.toString() + " !");
+				}
+				break;
+			case 0:
+				ajouterEquipement(equipement);
+				System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.toString() + " !");
+				break;
+			default:
+				break;
+		}
+	}
+	
+	private void ajouterEquipement(Equipement equipement) {
+		equipements[nbEquipements++] = equipement;
 	}
 	
 }
