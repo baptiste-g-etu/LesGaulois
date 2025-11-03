16a951fc89275cf4bd99574eebfb59eff3678532
Nerf Magicarpe <baptiste.goudet@gmail.com>
2025-11-03 18:16:19 +0100

    Add TP2 - Partie 2 Post/Precondition

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -9,6 +9,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified(force);
 	}
 	//Getter
 	public String getNom() {
@@ -24,6 +25,10 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		// Pré condition
+		assert isInvariantVerified(force);
+		final int force_debut = force;
+		
 		force -= forceCoup;
 		if(force < 1) {
 			force = 0;
@@ -31,5 +36,24 @@ public class Romain {
 			
 		}else
 			parler("Aïe !");
+		
+		// Post condition
+		assert isInvariantVerified(force);
+		assert force < force_debut;
+		System.out.println("Post condition vérifiée");
+		
 	}
+	
+	
+	// ----------- TP 2 -----------
+	private boolean isInvariantVerified(int force) {
+		return force >= 0;
+	}
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
+		Gaulois asterix = new Gaulois("Astérix", 4);
+		asterix.frapper(minus);
+	}
+	
 }
